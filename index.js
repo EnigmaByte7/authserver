2281a44d2fc869267ab4a8aae625ceb64ca08a29
EnigmaByte7 <saxenay117@gmail.com>
2025-12-28 00:15:42 +0530

    state_fix

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ import { auth } from "./lib/auth.js";
 const app = express();
 const port = 8080;
 
+app.use((req, res, next) => {
+  if (req.url.startsWith("/api/auth")) {
+    console.log("🟣 BETTER-AUTH ROUTE HIT:", req.method, req.url);
+    console.log("Query Params:", req.query);
+    console.log("Cookies from App/Google:", req.headers.cookie);
+  }
+  next();
+});
+
+
 app.all('/api/auth/{*any}', toNodeHandler(auth));
 console.log('hitting server');
 
